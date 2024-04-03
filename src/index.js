c6276fbfd7fec297be672da793fb38130596db97
Tony Varghese <varghesetony901@gmail.com>
2024-04-03 20:02:46 +0530

    react-snap removed

--- src/index.js
+++ src/index.js
@@ -1,37 +1,43 @@
 import React from "react";
-import { hydrate, render } from "react-dom";
+import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./app/store";
 import "./index.css";
 
-// const rootElement = ReactDOM.createRoot(document.getElementById('root'));
-const rootElement = document.getElementById("root");
 
-const APP = (
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <HelmetProvider>
         <App />
       </HelmetProvider>
     </Provider>
+
   </React.StrictMode>
 );
 
-if (rootElement.hasChildNodes()) {
-  hydrate(APP, rootElement);
-} else {
-  render(APP, rootElement);
-}
 
-// root.render(
+// const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+// const rootElement = document.getElementById("root");
+
+// const APP = (
 //   <React.StrictMode>
 //     <Provider store={store}>
 //       <HelmetProvider>
 //         <App />
 //       </HelmetProvider>
 //     </Provider>
-
 //   </React.StrictMode>
 // );
+
+// if (rootElement.hasChildNodes()) {
+//   hydrate(APP, rootElement);
+// } else {
+//   render(APP, rootElement);
+// }
